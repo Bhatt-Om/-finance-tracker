914c2e339b056a335d9e95ad7ab0ab324b3753c5
Bhatt-Om <bhattom1111@gmail.com>
2023-05-17 14:26:18 +0530

    add fav-icon gem and create search form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def favicon_link_tag(source = "favicon.ico", options = {})
+    tag("link", {
+      rel: "shortcut icon",
+      type: "image/x-icon",
+      href: path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
+    }.merge!(options.symbolize_keys))
+  end
 end
