eb9e71547fe4d641c081cfd203671a205bb97c96
Bhatt-Om <bhattom1111@gmail.com>
2023-05-17 14:25:03 +0530

    add user controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'my_portfolio', to: 'users#my_portfolio'
   root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
