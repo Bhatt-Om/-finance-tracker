edc3ef81d34e971e72117a7ec9d3cf34ece05ac6
Bhatt-Om <bhattom1111@gmail.com>
2023-05-17 18:36:13 +0530

    add users stock model and controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :user_stocks
   devise_for :users
+  get 'search_stock', to: 'stocks#search'
   get 'my_portfolio', to: 'users#my_portfolio'
   root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
